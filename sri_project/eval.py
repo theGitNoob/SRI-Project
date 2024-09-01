f93e59d9b0b1c02e10526c1946bc831baf0ab070
Rafael Acosta <theGitNoob@gmail.com>
2024-09-01 21:37:18 +0000

    feat: add match result ratio comparation

---
+++ sri_project/eval.py
@@ -0,0 +1,6 @@
+def exact_match_ratio(answers: list[int]) -> float:
+    count = 0
+    for idx,ans in enumerate(answers):
+        if idx == ans:
+            count+=1
+    return count/len(answers)
